7caf50cb221a5b78eb8308fbad281791ba5330f4
jnrpobee <35383366+jnrpobee@users.noreply.github.com>
2018-03-24 12:48:52 +0000

    Add files via upload

--- assignments/weathersite/scripts/homejson.js
+++ assignments/weathersite/scripts/homejson.js
@@ -1,5 +1,3 @@
-
-    //var header = document.querySelector('header');
     var section = document.querySelector('section');
     
         var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
@@ -10,27 +8,14 @@ request.send();
         
         request.onload = function() {
   var townData = request.response;
- // populateHeader(townData);
   showData(townData);
 }
-        // this part will fill the <header> with the correct data,
-        
-      /*  function populateHeader(jsonObj) {
-  var myH1 = document.createElement('h1');
-  myH1.textContent = jsonObj['squadName'];
-  header.appendChild(myH1);
-
-  var myPara = document.createElement('p');
-  myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-  header.appendChild(myPara);
-}*/
-
-// this part will create an information card for each town, and insert it into the <section>.
-        
+    
         function showData(jsonObj) {
   var index = jsonObj['towns'];
       
   for (var i = 0; i < index.length; i++) {
+       if (i === 2) { continue; }
     var myArticle = document.createElement('article');
     var myH2 = document.createElement('h2');
     var myPara1 = document.createElement('p');
@@ -38,14 +23,14 @@ request.send();
     var myPara3 = document.createElement('p');
     var myPara4 = document.createElement('p');
     var myPara5 = document.createElement('p');
-    var myList = document.createElement('ul.eat');
+    var myList = document.createElement('ul');
 
     myH2.textContent = index[i].name;
     myPara1.textContent = 'Motto: ' + index[i].motto;
     myPara2.textContent = 'Year founded: ' + index[i].yearFounded;
     myPara3.textContent = 'Current population: ' + index[i].currentPopulation;
     myPara4.textContent = 'Average rainfall: ' + index[i].averageRainfall;
-    myPara5.textContent = 'Events:';
+    myPara5.textContent = 'Events: ';
         
     var Events = index[i].events;
     for (var j = 0; j < Events.length; j++) {
--- assignments/weathersite/scripts/wu-weather.js
+++ assignments/weathersite/scripts/wu-weather.js
@@ -35,38 +35,4 @@ request.onload = function () {
     
     document.getElementById('relativE').innerHTML = franklinWeather.current_observation.relative_humidity;
     
- //for a period of 7days   
-    
-    document.getElementById('daY1').innerHTML = franklinWeather.forecast.txt_forecast.forecastday['1'].fcttext;
-    document.getElementById('iconD1').src = franklinWeather.forecast.txt_forecast.forecastday['1'].icon_url;
-    
-    document.getElementById('daY2').innerHTML = franklinWeather.forecast.txt_forecast.forecastday['2'].fcttext;
-    document.getElementById('iconD2').src = franklinWeather.forecast.txt_forecast.forecastday['2'].icon_url;
-    
-    document.getElementById('daY3').innerHTML = franklinWeather.forecast.txt_forecast.forecastday['3'].fcttext;
-    document.getElementById('iconD3').src = franklinWeather.forecast.txt_forecast.forecastday['3'].icon_url;
-    
-    document.getElementById('daY4').innerHTML = franklinWeather.forecast.txt_forecast.forecastday['4'].fcttext;
-    document.getElementById('iconD4').src = franklinWeather.forecast.txt_forecast.forecastday['4'].icon_url;
-    
-    document.getElementById('daY5').innerHTML = franklinWeather.forecast.txt_forecast.forecastday['5'].fcttext;
-    document.getElementById('iconD5').src = franklinWeather.forecast.txt_forecast.forecastday['5'].icon_url;
-    
-    document.getElementById('daY6').innerHTML = franklinWeather.forecast.txt_forecast.forecastday['6'].fcttext;
-    document.getElementById('iconD6').src = franklinWeather.forecast.txt_forecast.forecastday['6'].icon_url;
-    
-    document.getElementById('daY7').innerHTML = franklinWeather.forecast.txt_forecast.forecastday['7'].fcttext;
-    document.getElementById('iconD7').src = franklinWeather.forecast.txt_forecast.forecastday['7'].icon_url;
-    
-    /*
-    document.getElementById('daY8').innerHTML = franklinWeather.forecast.txt_forecast.forecastday['8'].fcttext;
-    document.getElementById('iconD8').src = franklinWeather.forecast.txt_forecast.forecastday['8'].icon_url;
-    
-    document.getElementById('daY9').innerHTML = franklinWeather.forecast.txt_forecast.forecastday['9'].fcttext;
-    document.getElementById('iconD9').src = franklinWeather.forecast.txt_forecast.forecastday['9'].icon_url;
-    
-    document.getElementById('daY10').innerHTML = franklinWeather.forecast.txt_forecast.forecastday['10'].fcttext;
-    document.getElementById('iconD10').src = franklinWeather.forecast.txt_forecast.forecastday['10'].icon_url;
-    -->*/
-    
 };
